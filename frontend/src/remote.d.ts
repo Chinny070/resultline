1441fb611ce941191275e84ca38ad902bed85315
Achinnys <lovelinchinelo065@gmail.com>
2026-09-21 09:37:33 +0100

    fix: restore stable StudioNet frontend SDK

--- frontend/src/remote.d.ts
+++ frontend/src/remote.d.ts
@@ -1,3 +1,2 @@
-declare module 'https://esm.sh/genlayer-js@2.0.0-rc.1';
-declare module 'https://esm.sh/genlayer-js@2.0.0-rc.1/chains';
-declare module 'https://esm.sh/@genlayer/transaction-kit@0.1.0-rc.2';
+declare module 'https://esm.sh/genlayer-js@1.1.8';
+declare module 'https://esm.sh/genlayer-js@1.1.8/chains';
